d2a6e3e69ffaaf9536e321c2e9ce56d22d91d96f
ALRuss <ALRuss@localhost>
2023-02-15 09:39:24 -0500

    Map class added

---
+++ RedGroup3DMaze/src/Map.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class Map {
+	
+	private int level;
+	private Maze maze;
+	private PlayerData player;
+	
+	Map(Maze maze, PlayerData player) {
+		this.maze = maze;
+		this.player = player;
+		level = player.getLevel();
+	}
+	
+	public void display(Graphics g) {
+		
+	}
+	
+	public void display(Graphics g, int newLevel) {
+		
+	}
+	
+}
