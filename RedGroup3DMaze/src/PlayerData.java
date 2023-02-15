662da6371af596c31b6ba1de429d4e8384f54b43
Bill-Boya-Wang <boya.wang@gmail.com>
2023-02-15 23:01:55 +0800

    PlayerData and Header class

---
+++ RedGroup3DMaze/src/PlayerData.java
@@ -0,0 +1,23 @@
+public class PlayerData {
+    private int[] coordinates = new int[3];
+    private char direction;
+    private int score;
+    public PlayerData(Maze maze) {
+        coordinates[0] = 0;
+        coordinates[1] = 0;
+        coordinates[2] = 3;
+        score = 0;
+    }
+
+    public int getCoordinate(char value) {
+        return coordinates[1];
+    }
+    public char getDirection() {
+        return direction;
+    }
+
+
+    public int getScore() {
+        return score;
+    }
+}
