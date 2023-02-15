ad3a3b51716acc65ae257c866657a6c8188ddc6b
maxbalkirsky <maxbalkirsky@MacBookAir7a2dec2e>
2023-02-14 23:36:02 -0500

    Unbelieveable - Max Test 2.0

--- RedGroup3DMaze/src/Run3DMaze.java
+++ RedGroup3DMaze/src/Run3DMaze.java
@@ -6,7 +6,8 @@ public class Run3DMaze {
 	}
 	
 	public static void main(String[]args) {
-		System.out.println("Red Team has been infiltrated!");
+		System.out.println("Genuinely this has been the worst experience of my life"
+		+ " Oh my heebie jeebus I could not imagine something worse!!!!");
 	}
 }
 //change 5
