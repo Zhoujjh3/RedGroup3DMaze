eed53238c6f3da6f0080c8a049550d431853172d
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-02-16 09:58:58 -0500

    PlayerData can move and return values.

--- RedGroup3DMaze/src/PlayerData.java
+++ RedGroup3DMaze/src/PlayerData.java
@@ -2,22 +2,53 @@ public class PlayerData {
     private int[] coordinates = new int[3];
     private char direction;
     private int score;
+    private int mazeSize;
     public PlayerData(Maze maze) {
         coordinates[0] = 0;
         coordinates[1] = 0;
-        coordinates[2] = 3;
+        coordinates[2] = 0;
+        direction = 'S';
         score = 0;
+        mazeSize = maze.getMazeSize();
     }
 
     public int getCoordinate(char value) {
-        return coordinates[1];
+        if (value == 'X') {
+        	return coordinates[0];
+        } else if (value == 'Y') {
+        	return coordinates[1];
+        } else if (value == 'Z') {
+        	return coordinates[2];
+        }
+        return -1;
     }
     public char getDirection() {
         return direction;
     }
-
-//<orderEntry type="jdk" jdkName="Android API 29 Platform" jdkType="Android SDK" />
     public int getScore() {
         return score;
+    }
+    public void setDirection(char newDir) {
+    	if (newDir == 'N' || newDir == 'E' || newDir == 'S' || newDir == 'W' || newDir == 'U' || newDir == 'D') {
+    		direction = newDir;
+    	}
+    }
+    public void setScore(int newScore) {
+    	score = newScore;
+    }
+    public void movePlayer(char direction) {
+    	if (direction == 'N' && coordinates[1] > 0) {
+    		coordinates[1]--;
+    	} else if (direction == 'E' && coordinates[0] < mazeSize-1) {
+    		coordinates[0]++;
+    	} else if (direction == 'S' && coordinates[1] < mazeSize-1) {
+    		coordinates[1]++;
+    	} else if (direction == 'W' && coordinates[0] > 0) {
+    		coordinates[0]--;
+    	} else if (direction == 'U' && coordinates[2] > 0) {
+    		coordinates[2]--;
+    	} else if (direction == 'D' && coordinates[2] < mazeSize-1) {
+    		coordinates[2]++;
+    	}
     }
 }
