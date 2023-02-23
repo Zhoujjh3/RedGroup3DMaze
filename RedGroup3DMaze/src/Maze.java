bbef95e89c77c7aaf27b8f31f57bac4ce1f0788d
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-02-23 15:23:32 -0500

    fullMazePathFind, untested

--- RedGroup3DMaze/src/Maze.java
+++ RedGroup3DMaze/src/Maze.java
@@ -44,7 +44,7 @@ public class Maze {
 		}
 		do {
 			setBaseMazeAndWalls(baseMaze, walls);
-			// creating the set path
+			createPath(baseMaze);
 			fillBaseMaze(baseMaze, walls);
 		} while (false /* while minMoves does not fit */);
 		return -1;
@@ -121,6 +121,33 @@ public class Maze {
 		char[][][] x = new char[1][baseMazeLevel.length][baseMazeLevel[0].length];
 		x[0] = baseMazeLevel;
 		return pathFind(x, startCoords, endCoords);
+	}
+	
+	private boolean fullMazePathFind(char[][][] baseMaze) {
+		int[] startCoords = new int[3];
+		int[] endCoords = new int[3];
+		for (int level=0; level<baseMaze.length; level++) {
+			// finding startCoords and endCoords
+			// note: WILL THROW ERROR IF LEVEL DOESN'T HAVE BOTH A 'U' AND A 'D'
+			startCoords[0] = level;
+			endCoords[0] = level;
+			for (int x=0; x<baseMaze[0].length; x++) {
+				for (int y=0; y<baseMaze[0][0].length; y++) {
+					if (baseMaze[level][x][y] == 'U' || baseMaze[level][x][y] == 'B') {
+						startCoords[1] = x;
+						startCoords[2] = y;
+					}
+					if (baseMaze[level][x][y] == 'D' || baseMaze[level][x][y] == 'B') {
+						endCoords[1] = x;
+						endCoords[2] = y;
+					}
+				}
+			}
+			if (!pathFind(baseMaze, startCoords, endCoords)) {
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	private char[][][] createPath(char[][][] baseMaze) {
