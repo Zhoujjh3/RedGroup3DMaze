0651099128d7320b93afeb6a3b7e2304c0d7f2bf
nimeister <nimeister@HN-1029-66308.hunter.local>
2023-02-24 10:06:36 -0500

    edited map class

--- RedGroup3DMaze/src/MapTester.java
+++ RedGroup3DMaze/src/MapTester.java
@@ -36,7 +36,7 @@ public class MapTester {
         });
         frame.setSize(1000,750);
         frame.setVisible(true);
-        frame.setResizable(true);
+        frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
     }
     
