91347fa27ea739d2142e0a8ecc829f96f7dc79ee
nimeister <nimeister@HN-1029-66308.hunter.local>
2023-03-01 13:36:43 -0500

    grid, hatch, trapdoor scaled, still errors

--- RedGroup3DMaze/src/MazeMap.java
+++ RedGroup3DMaze/src/MazeMap.java
@@ -7,7 +7,8 @@ public class MazeMap {
 	private ManualTestMaze maze;
 	private PlayerData player;
 	private int size;
-	private int width;
+	private double width;
+	private double scale;
 	
 	MazeMap(ManualTestMaze maze, PlayerData player) {
 		this.maze = maze;
@@ -15,10 +16,11 @@ public class MazeMap {
 		level = this.player.getCoordinate('Z');
 		size = this.maze.getMazeSize();
 		if (size==4) {
-			width = 166;
+			width = 166.25;
 		} else {
 			width = 133;
 		}
+		
 	}
 	
 	public void display(Graphics g, int newLevel) {
@@ -27,7 +29,7 @@ public class MazeMap {
 		int length = size;
 		for (int i=0; i<length; i++) {
 			for (int o=0; o<length; o++) {
-				Room room = maze.getRoom(newLevel, o, i);
+				Room room = maze.getRoom(newLevel, o, i);//FIX, MICHAEL CHANGED THE ORDER
 				int[] coord = {i,o};
 				if (room.getDirection('N')) {
 					painter.displayDoorN(g, coord);
@@ -69,9 +71,9 @@ public class MazeMap {
 			}
 		}
 		g.setColor(Color.white);
-        g.setFont(new Font("TimesRoman", Font.PLAIN, 15));
-		Integer newlevel = newLevel;
-		g.drawString("Displayed level: " + newlevel.toString(), 625, 735);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, (int)(15*scale)));
+		Integer newlevel = newLevel+1;
+		g.drawString("Displayed level: " + newlevel.toString(), (int)(625*scale), (int)(735*scale));
 	}
 	
 	public class Painter {
@@ -80,79 +82,83 @@ public class MazeMap {
 			
 			//Header placeholder
 	        g.setColor(Color.gray);
-	        g.fillRect(0,0,1000,40);
+	        g.fillRect(0, 0, (int)(1000*scale), (int)(40*scale));
 			
 	        //Footer placeholder
 	        g.setColor(Color.gray);
-	        g.fillRect(0, 710, 1000, 40);
+	        g.fillRect(0, (int)(710*scale), (int)(1000*scale), (int)(40*scale));
 	        
 	        //Grid outline
 			g.setColor(Color.black);
-			g.fillRect(165, 40, 670, 5);
-			g.fillRect(165, 705, 670, 5);
-			g.fillRect(165, 40, 5, 670);
-			g.fillRect(830, 40, 5, 670);
+			g.fillRect((int)(165*scale), (int)(40*scale), (int)(670*scale), (int)(5*scale));
+			g.fillRect((int)(165*scale), (int)(705*scale), (int)(670*scale), (int)(5*scale));
+			g.fillRect((int)(165*scale), (int)(40*scale), (int)(5*scale), (int)(670*scale));
+			g.fillRect((int)(830*scale), (int)(40*scale), (int)(5*scale), (int)(670*scale));
 			
 			if (size==4) {
 				//4x4 grid
 				//vertical
-				g.fillRect(165+width, 40, 5, 665);
-				g.fillRect(165+width*2, 40, 5, 665);
-				g.fillRect(165+width*3, 40, 5, 665);
+				g.fillRect((int)(165*scale+width), (int)(40*scale), (int)(5*scale), 665);
+				g.fillRect((int)(165*scale+width*2), (int)(40*scale), (int)(5*scale), 665);
+				g.fillRect((int)(165*scale+width*3), (int)(40*scale), (int)(5*scale), 665);
 				
 				//horizontal
-				g.fillRect(165, 40+width, 670, 5);
-				g.fillRect(165, 40+width*2, 670, 5);
-				g.fillRect(165, 40+width*3, 670, 5);
+				g.fillRect((int)(165*scale), (int)(40*scale+width), (int)(670*scale), (int)(5*scale));
+				g.fillRect((int)(165*scale), (int)(40*scale+width*2), (int)(670*scale), (int)(5*scale));
+				g.fillRect((int)(165*scale), (int)(40*scale+width*3), (int)(670*scale), (int)(5*scale));
 			} else {
 				//5x5 grid
 				//vertical
-				g.fillRect(165+width, 40, 5, 665);
-				g.fillRect(165+width*2, 40, 5, 665);
-				g.fillRect(165+width*3, 40, 5, 665);
-				g.fillRect(165+width*4, 40, 5, 665);
+				g.fillRect((int)(165*scale+width), (int)(40*scale), (int)(5*scale), (int)(665*scale));
+				g.fillRect((int)(165*scale+width*2), (int)(40*scale), (int)(5*scale), (int)(665*scale));
+				g.fillRect((int)(165*scale+width*3), (int)(40*scale), (int)(5*scale), (int)(665*scale));
+				g.fillRect((int)(165*scale+width*4), (int)(40*scale), (int)(5*scale), (int)(665*scale));
 				
 				//horizontal
-				g.fillRect(165, 40+width, 670, 5);
-				g.fillRect(165, 40+width*2, 670, 5);
-				g.fillRect(165, 40+width*3, 670, 5);
-				g.fillRect(165, 40+width*4, 670, 5);
+				g.fillRect((int)(165*scale), (int)(40*scale+width), (int)(670*scale), (int)(5*scale));
+				g.fillRect((int)(165*scale), (int)(40*scale+width*2), (int)(670*scale), (int)(5*scale));
+				g.fillRect((int)(165*scale), (int)(40*scale+width*3), (int)(670*scale), (int)(5*scale));
+				g.fillRect((int)(165*scale), (int)(40*scale+width*4), (int)(670*scale), (int)(5*scale));
 			}
 			
 			//Outside area
 			g.setColor(Color.darkGray);
-			g.fillRect(0, 40, 165, 670);
-			g.fillRect(835, 40, 165, 670);
+			g.fillRect(0, (int)(40*scale), (int)(165*scale), (int)(670*scale));
+			g.fillRect((int)(835*scale), (int)(40*scale), (int)(165*scale), (int)(670*scale));
 		}
 		public void displayTrap(Graphics g, int[] coord) {
-			int startX = coord[0]*width;
-			int startY = coord[1]*width;
+			double startX = coord[0]*width;
+			double startY = coord[1]*width;
 			int[] trapX = {180, 250, 320, 320, 250, 180, 180};
 			int[] trapY = {165, 180, 165, 180, 195, 180, 165};
-			/*if (size==5) {
-				trapX = {};
-				trapY = {};
-			}*/
+			if (size==5) {
+				int[] tempTrapX = {(int)(180*.8), (int)(250*.8), (int)(320*.8), (int)(320*.8), (int)(250*.8), (int)(180*.8), (int)(180*.8)};
+				trapX = tempTrapX;
+				int[] tempTrapY = {(int)(165*.8), (int)(180*.8), (int)(165*.8), (int)(180*.8), (int)(195*.8), (int)(180*.8), (int)(165*.8)};
+				trapY = tempTrapY;
+			}
 			for (int i=0; i<7; i++) {
-				trapX[i] = trapX[i] + startX;
-				trapY[i] = trapY[i] + startY;
+				trapX[i] = (int)((trapX[i] + startX)*scale);
+				trapY[i] = (int)((trapY[i] + startY)*scale);
 			}
 			Color trapColor = new Color(255, 128, 0);
 			g.setColor(trapColor);
 			g.fillPolygon(trapX, trapY, 7);
 		}
 		public void displayHatch(Graphics g, int[] coord) {
-			int startX = coord[0]*width;
-			int startY = coord[1]*width;
+			double startX = coord[0]*width;
+			double startY = coord[1]*width;
 			int[] hatchX = {180, 250, 320, 320, 250, 180, 180};
 			int[] hatchY = {70, 55, 70, 85, 70, 85, 70};
-			/*if (size==5) {
-				trapX = {};
-				trapY = {};
-			}*/
+			if (size==5) {
+				int[] tempHatchX = {(int)(180*.8), (int)(250*.8), (int)(320*.8), (int)(320*.8), (int)(250*.8), (int)(180*.8), (int)(180*.8)};
+				hatchX = tempHatchX;
+				int[] tempHatchY = {(int)(70*.8), (int)(55*.8), (int)(70*.8), (int)(85*.8), (int)(70*.8), (int)(85*.8), (int)(50*.8)};
+				hatchY = tempHatchY;
+			}
 			for (int i=0; i<7; i++) {
-				hatchX[i] = hatchX[i] + startX;
-				hatchY[i] = hatchY[i] + startY;
+				hatchX[i] = (int)((hatchX[i] + startX)*scale);
+				hatchY[i] = (int)((hatchY[i] + startY)*scale);
 			}
 			Color hatchColor = new Color(0, 0, 255);
 			g.setColor(hatchColor);
