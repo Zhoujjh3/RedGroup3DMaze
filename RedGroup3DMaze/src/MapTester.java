b3a54a2991d9943d521c9041c7cb7025b19a45f5
Bill-Boya-Wang <boya.wang@gmail.com>
2023-03-03 10:05:44 -0500

    fixed taht error in maptester

--- RedGroup3DMaze/src/MapTester.java
+++ RedGroup3DMaze/src/MapTester.java
@@ -87,7 +87,7 @@ public class MapTester {
                 levelDown.setVisible(false);
                 levelUp.setVisible(false);
             }
-            header.display(g, 1);
+            header.display(g, this.getSize());
         }
     }
 
