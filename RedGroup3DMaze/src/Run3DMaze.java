1a7f2d0d7d02def7aeed74f1d4913aada8aff6fa
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-03-06 10:03:58 -0500

    Run3DMaze outline

--- RedGroup3DMaze/src/Run3DMaze.java
+++ RedGroup3DMaze/src/Run3DMaze.java
@@ -7,8 +7,13 @@ public class Run3DMaze {
 	
 	JFrame screen;
 	JPanel gamePanel;
+	Selection selectionScreen;
+	PlayerData player;
 	Maze maze;
-	enum mazeState{
+	MazeMap map;
+	Leaderboard leaderboard;
+	mazeState state;
+	public enum mazeState{
 		WELCOMESCREEN,
 		CHAMBERVIEW,
 		MAPVIEW,
@@ -16,10 +21,19 @@ public class Run3DMaze {
 	}
 	
 	public Run3DMaze() {
+		
+	}
+	
+	public void runWelcomeScreen() {
 		screen = new JFrame();
 		gamePanel = new GamePanel();
 		gamePanel.setPreferredSize(new Dimension(1000, 750));
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		state = mazeState.WELCOMESCREEN;
+		selectionScreen = new Selection();
+		// leaderboard = new Leaderboard();
+		
 		screen.setContentPane(gamePanel);
 		screen.pack();
 	    screen.setVisible(true);
@@ -28,7 +42,32 @@ public class Run3DMaze {
 		//comment for max
 	}
 	
+	public void runMaze() {
+		// initialize maze, map, and playerData
+		
+		
+		// runChamberView();
+	}
+	
+	public void runChamberView() {
+		state = mazeState.CHAMBERVIEW;
+		
+		
+	}
+	
+	public void runMapView() {
+		state = mazeState.MAPVIEW;
+		
+		
+	}
+	
+	public void runLeaderboard() {
+		state = mazeState.LEADERBOARD;
+		
+		
+	}
+	
 	public static void main(String[]args) {
-		new Run3DMaze();
+		new Run3DMaze().runWelcomeScreen();
 	}
 }
