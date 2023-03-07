60f733cd19f232a91467bd00e95de90617dc1422
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-03-07 09:44:29 -0500

    Mistake, it's all good

--- RedGroup3DMaze/src/PlayerData.java
+++ RedGroup3DMaze/src/PlayerData.java
@@ -12,7 +12,7 @@ public class PlayerData {
         mazeSize = size;
     }
 
-    public int getCoordinate(char value) { // please work well
+    public int getCoordinate(char value) {
         if (value == 'X') {
         	return coordinates[0];
         } else if (value == 'Y') {
