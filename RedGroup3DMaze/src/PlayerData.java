376be3760851057ec7d921f0fc62a80713a6d8b0
mparke2914 <125300129+mparke2914@users.noreply.github.com>
2023-03-07 09:42:17 -0500

    Changed score to moves

--- RedGroup3DMaze/src/PlayerData.java
+++ RedGroup3DMaze/src/PlayerData.java
@@ -1,14 +1,14 @@
 public class PlayerData {
     private int[] coordinates = new int[3];
     private char direction;
-    private int score;
+    private int moves;
     private int mazeSize;
     public PlayerData(int size) {
         coordinates[0] = 0;
         coordinates[1] = 0;
         coordinates[2] = 0;
         direction = 'S';
-        score = 0;
+        moves = 0;
         mazeSize = size;
     }
 
@@ -25,16 +25,16 @@ public class PlayerData {
     public char getDirection() {
         return direction;
     }
-    public int getScore() {
-        return score;
+    public int getMoves() {
+        return moves;
     }
     public void setDirection(char newDir) {
     	if (newDir == 'N' || newDir == 'E' || newDir == 'S' || newDir == 'W' || newDir == 'U' || newDir == 'D') {
     		direction = newDir;
     	}
     }
-    public void setScore(int newScore) {
-    	score = newScore;
+    public void setMoves(int newMoves) {
+    	moves = newMoves;
     }
     public void movePlayer(char direction) {
     	if (direction == 'N' && coordinates[1] > 0) {
@@ -50,5 +50,6 @@ public class PlayerData {
     	} else if (direction == 'D' && coordinates[2] < mazeSize-1) {
     		coordinates[2]++;
     	}
+    	moves++;
     }
 } // Testing
