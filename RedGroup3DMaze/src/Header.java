7a9c286f99215b5ba548015b5af40281b22ae9f1
faculty <faculty@10.129.227.69>
2023-06-04 20:53:33 -0400

    header font

--- RedGroup3DMaze/src/Header.java
+++ RedGroup3DMaze/src/Header.java
@@ -6,7 +6,8 @@ import javax.imageio.ImageIO;
 public class Header {
     private boolean chamberView;
     private PlayerData player;
-    private Image X, Y, PlayerLevel, Moves;
+    private Image X, Y, PlayerLevel, Moves, Direction;
+    private Image[] digits, directions;
     public Header(Maze maze, PlayerData player) {
         this.player = player;
         chamberView = true;
@@ -15,6 +16,16 @@ public class Header {
 			Y = ImageIO.read(getClass().getClassLoader().getResource("Y.png"));
 			PlayerLevel = ImageIO.read(getClass().getClassLoader().getResource("Player Level.png"));
 			Moves = ImageIO.read(getClass().getClassLoader().getResource("Moves.png"));
+			Direction = ImageIO.read(getClass().getClassLoader().getResource("Direction.png"));
+			digits = new Image[10];
+			for (int i=0; i<10; i++) {
+				digits[i] = ImageIO.read(getClass().getClassLoader().getResource(String.valueOf(i)+".png"));
+			}
+			directions = new Image[4];
+			directions[0] = ImageIO.read(getClass().getClassLoader().getResource("N.png"));
+			directions[1] = ImageIO.read(getClass().getClassLoader().getResource("e.png"));
+			directions[2] = ImageIO.read(getClass().getClassLoader().getResource("s.png"));
+			directions[3] = ImageIO.read(getClass().getClassLoader().getResource("w.png"));
 		} catch (IOException e) {
 			System.out.println("Background image not found.");
 			e.printStackTrace();
@@ -26,15 +37,35 @@ public class Header {
         g.fillRect(0,0,(int)(scaleFactor.width),(int)(40 * scaleFactor.height/750));
         g.setColor(Color.white);
         g.setFont(new Font("TimesRoman", Font.PLAIN, (int)(15* scaleFactor.height/750)));
-        //g.drawString("X: " + (player.getCoordinate('X') + 1), (int)(20* scaleFactor.width/1000), (int)(25 * scaleFactor.height/750));
-        g.drawImage(X, (int)(25*scaleFactor.width/1000-100), (int)(10 * scaleFactor.height/750 - 7), null);
-        //g.drawString("Y: " + (player.getCoordinate('Y') + 1), (int)(60 * scaleFactor.width/1000), (int)(25 * scaleFactor.height/750));
-        g.drawImage(Y, (int)(65*scaleFactor.width/1000-100), (int)(10 * scaleFactor.height/750 - 7), null);
-        //g.drawString("Player Level: " + (player.getCoordinate('Z') + 1), (int)(200 * scaleFactor.width/1000), (int)(25 * scaleFactor.height/750));
-        g.drawImage(PlayerLevel, (int)(205*scaleFactor.width/1000-140), (int)(10 * scaleFactor.height/750-7), null);
-        //g.drawString("Moves: " + player.getMoves(), (int)(400 * scaleFactor.width/1000), (int)(25 * scaleFactor.height/750));
-        g.drawImage(Moves, (int)(405*scaleFactor.width/1000-140), (int)(10 * scaleFactor.height/750 - 7), null);
-        g.drawString("Direction: " + Character.toString(player.getDirection()), (int)(550 * scaleFactor.width/1000), (int)(25 * scaleFactor.height/750));
+        
+        g.drawImage(X, (int)(60*scaleFactor.width/1000-100), (int)(10 * scaleFactor.height/750 - 7), null);
+        g.drawImage(digits[player.getCoordinate('X') + 1], (int)(65*scaleFactor.width/1000), (int)(12 * scaleFactor.height/750), null);
+        
+        g.drawImage(Y, (int)(120*scaleFactor.width/1000-100), (int)(10 * scaleFactor.height/750 - 7), null);
+        g.drawImage(digits[player.getCoordinate('Y') + 1], (int)(125*scaleFactor.width/1000), (int)(12 * scaleFactor.height/750), null);
+        
+        g.drawImage(PlayerLevel, (int)(340*scaleFactor.width/1000-140), (int)(10 * scaleFactor.height/750-7), null);
+        g.drawImage(digits[player.getCoordinate('Z') + 1], (int)(345*scaleFactor.width/1000), (int)(12 * scaleFactor.height/750), null);
+        
+        g.drawImage(Moves, (int)(500*scaleFactor.width/1000-140), (int)(10 * scaleFactor.height/750 - 7), null);
+        String digit = String.valueOf(player.getMoves()).substring(0,1);
+        g.drawImage(digits[Integer.parseInt(digit)], (int)((505)*scaleFactor.width/1000), (int)(12 * scaleFactor.height/750), null);
+    	for (int i = 1; i<String.valueOf(player.getMoves()).length(); i++) {
+    		digit = String.valueOf(player.getMoves()).substring(i, i+1);
+    		g.drawImage(digits[Integer.parseInt(digit)], (int)((505+13*i)*scaleFactor.width/1000), (int)(12 * scaleFactor.height/750), null);
+    	}
+    	
+        //g.drawString("Direction: " + Character.toString(player.getDirection()), (int)(550 * scaleFactor.width/1000), (int)(25 * scaleFactor.height/750));
+        g.drawImage(Direction, (int)(720*scaleFactor.width/1000-140), (int)(10 * scaleFactor.height/750 - 7), null);
+        int num = 3;
+        if (player.getDirection()=='N') {
+        	num = 0;
+        } else if (player.getDirection()=='E') {
+        	num = 1;
+        } else if (player.getDirection()=='S') {
+        	num = 2;
+        }
+        g.drawImage(directions[num], (int)((738)*scaleFactor.width/1000-100), (int)(10 * scaleFactor.height/750-7), null);
     }
     public void update() {
 
--- RedGroup3DMaze/src/HeaderTester.java
+++ RedGroup3DMaze/src/HeaderTester.java
@@ -15,8 +15,8 @@ public class HeaderTester {
         frame = new JFrame("header tester");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000,750);
-        panel = new Panel(); 
-        //changeView = new JButton(header.getView());
+        panel = new Panel();
+        //183
         changeView = new JButton(new ImageIcon(new ImageIcon(getClass().getClassLoader().
 				getResource("MAP Button.png")).getImage().
 				getScaledInstance(80, 24, 
@@ -25,8 +25,9 @@ public class HeaderTester {
         changeView.setContentAreaFilled(false);
         changeView.setBorderPainted(false);
         panel.setLayout(null);
-        changeView.setBounds(740, 5, 100, 30);
-       // changeView.setBounds((int)(740 * frame.getSize().width/1000), (int)(5* frame.getSize().height/750), (int)(100 * frame.getSize().width/1000), (int)(30 * frame.getSize().height/750));
+        //191
+        changeView.setBounds(820, 5, 100, 30);
+        //195
         changeView.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 header.changeView();
@@ -42,19 +43,17 @@ public class HeaderTester {
         changeView.setContentAreaFilled(false);
         changeView.setBorderPainted(false);
 
-        /* Size and then display the frame. */
-
         frame.setVisible(true);
         frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
-		//comment
     }
 
     class Panel extends JPanel{
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             header.display(g ,this.getSize());
-            changeView.setBounds((int)(740 * frame.getSize().width/1000), (int)(5* frame.getSize().height/750), (int)(100 * frame.getSize().width/1000), (int)(30 * frame.getSize().height/750));
+            //121, 109 GamePanel
+            changeView.setBounds((int)(820 * frame.getSize().width/1000), (int)(5* frame.getSize().height/750), (int)(100 * frame.getSize().width/1000), (int)(30 * frame.getSize().height/750));
 
         }
     }
