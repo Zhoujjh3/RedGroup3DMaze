20af2ea834723fa6fa64dd46482bf194e887dc6c
Wenge <Wenge@HN-1029-66308.hunter.local>
2023-06-08 09:51:03 -0400

    selection buttons

--- RedGroup3DMaze/src/Selection.java
+++ RedGroup3DMaze/src/Selection.java
@@ -1,5 +1,3 @@
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -27,9 +25,15 @@ public class Selection extends JPanel implements ActionListener{
 		this.panel = panel;
 		panel.setLayout(null);
 		background = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("selBackground.png")));	
-		easyButt = new JButton("Easy");
-		mediumButt = new JButton("Medium");
-		hardButt = new JButton("Hard");
+		easyButt = new JButton(new ImageIcon(new ImageIcon(getClass().getClassLoader().
+				getResource("easy.png")).getImage().
+				getScaledInstance(400, 100, java.awt.Image.SCALE_SMOOTH)));
+		mediumButt = new JButton(new ImageIcon(new ImageIcon(getClass().getClassLoader().
+				getResource("medium.png")).getImage().
+				getScaledInstance(400, 100, java.awt.Image.SCALE_SMOOTH)));
+		hardButt = new JButton(new ImageIcon(new ImageIcon(getClass().getClassLoader().
+				getResource("hard.png")).getImage().
+				getScaledInstance(400, 100, java.awt.Image.SCALE_SMOOTH)));
 		logo = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("MazeLogo.png")));
 		title = new JLabel(new ImageIcon(getClass().getClassLoader().getResource("Welcome.png")));	
 	}
@@ -43,32 +47,46 @@ public class Selection extends JPanel implements ActionListener{
 //	    background.setBounds(0, 0, frame.getWidth(), frame.getHeight());
 //	    background.setOpaque(true);
 		
-	    easyButt.setFont(new Font("Serif", Font.PLAIN, 30));
-	    easyButt.setBackground(Color.green);
-//	    easyButt.setOpaque(true);
 	    easyButt.setActionCommand("Easy");
 	    easyButt.addActionListener(this);
 	    
-	    mediumButt.setFont(new Font("Serif", Font.PLAIN, 30));
-	    mediumButt.setBackground(Color.yellow);
-//	    mediumButt.setOpaque(true);
 	    mediumButt.setActionCommand("Medium");
 	    mediumButt.addActionListener(this);
 	    
-	    hardButt.setFont(new Font("Serif", Font.PLAIN, 30));
-	    hardButt.setBackground(Color.red);
-//	    hardButt.setOpaque(true);
 	    hardButt.setActionCommand("Hard");
 	    hardButt.addActionListener(this);
 	    
+	    easyButt.setOpaque(false);
+	    easyButt.setContentAreaFilled(false);
+	    easyButt.setBorderPainted(false);
+	    mediumButt.setOpaque(false);
+	    mediumButt.setContentAreaFilled(false);
+	    mediumButt.setBorderPainted(false);
+	    hardButt.setOpaque(false);
+	    hardButt.setContentAreaFilled(false);
+	    hardButt.setBorderPainted(false);
+	    
 	    int panelWidth = panel.getSize().width;
 	    int panelHeight = panel.getSize().height;
-	    		
-	    easyButt.setBounds((int)(panelWidth*.07), (int)(panelHeight*0.267),(int)(panelWidth*.4),(int)(panelHeight*.1));
+	    
+	    easyButt.setIcon(new ImageIcon(new ImageIcon(getClass().getClassLoader().
+				getResource("easy.png")).getImage().
+				getScaledInstance((int)(400 * panelWidth/1000), (int)(100* panelHeight/750), 
+						java.awt.Image.SCALE_SMOOTH)));
+	    mediumButt.setIcon(new ImageIcon(new ImageIcon(getClass().getClassLoader().
+				getResource("medium.png")).getImage().
+				getScaledInstance((int)(400 * panelWidth/1000), (int)(100* panelHeight/750), 
+						java.awt.Image.SCALE_SMOOTH)));
+	    hardButt.setIcon(new ImageIcon(new ImageIcon(getClass().getClassLoader().
+				getResource("hard.png")).getImage().
+				getScaledInstance((int)(400 * panelWidth/1000), (int)(100* panelHeight/750), 
+						java.awt.Image.SCALE_SMOOTH)));
+	    
+	    easyButt.setBounds((int)(panelWidth*.07), (int)(panelHeight*0.267),(int)(400*panelWidth/1000),(int)(110*panelHeight/750));
 	    easyButt.setFont(new Font("Serif", Font.PLAIN, (int)(panelWidth*.03)));
-	    mediumButt.setBounds((int)(panelWidth*.07), (int)(panelHeight*0.467), (int)(panelWidth*.4),(int)(panelHeight*.1));
+	    mediumButt.setBounds((int)(panelWidth*.07), (int)(panelHeight*0.467), (int)(400*panelWidth/1000),(int)(110*panelHeight/750));
 	    mediumButt.setFont(new Font("Serif", Font.PLAIN, (int)(panelWidth*.03)));
-	    hardButt.setBounds((int)(panelWidth*.07), (int)(panelHeight*0.667), (int)(panelWidth*.4),(int)(panelHeight*.1));
+	    hardButt.setBounds((int)(panelWidth*.07), (int)(panelHeight*0.667), (int)(400*panelWidth/1000),(int)(110*panelHeight/750));
 	    hardButt.setFont(new Font("Serif", Font.PLAIN, (int)(panelWidth*.03)));
 	    	    
 	    title.setBounds((int)(panelWidth*0.01), (int)(panelHeight*.06), (int)(panelWidth*1), (int)(panelHeight*.13));
